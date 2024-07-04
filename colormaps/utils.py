1219a737e38dfc55bf6a969f6d8b6b44a1495d8a
pratiman-91 <pratiman_patel@hotmail.com>
2024-07-04 21:31:05 +0800

    colormaps updated to matplotlib version 3.9.0 along with new scientific maps version 8.0.1

--- colormaps/utils.py
+++ colormaps/utils.py
@@ -1,35 +1,42 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from .colormap import Colormap
+
 # import matplotlib.colors as mc
 import matplotlib.cm
 import numpy as np
 import os
 from glob import glob
 
-def concat(cnames, ratios=None, discrete=256, name='concat', save=False):
+mlp_version = matplotlib.__version__
+
+from ._version import __version__
+from packaging import version
+
+
+def concat(cnames, ratios=None, discrete=256, name="concat", save=False):
     """
-        Concatenate two or more colormaps.
+    Concatenate two or more colormaps.
 
-        Parameters
-        ----------
-        cnames : list of str or list of cmaps
-            Provide the possible list of cmaps.
+    Parameters
+    ----------
+    cnames : list of str or list of cmaps
+        Provide the possible list of cmaps.
 
-        ratios : list of floats
-            Divide the colorbar based on the ratios. By default, divide it into the equal number of parts.
-            The sum of the ratios should be 1.
-        
-        discrete : int
-            Discrete number of levels. (Default: 256)
+    ratios : list of floats
+        Divide the colorbar based on the ratios. By default, divide it into the equal number of parts.
+        The sum of the ratios should be 1.
 
-        name : str
-            Name of the file. The file will be saved in rgb format
+    discrete : int
+        Discrete number of levels. (Default: 256)
 
-        save : boolean (True or False)  
-            If you want to save your colomap, them use True and must provide a name.
+    name : str
+        Name of the file. The file will be saved in rgb format
 
-        """
+    save : boolean (True or False)
+        If you want to save your colomap, them use True and must provide a name.
+
+    """
     import colormaps as cmaps
 
     # Two variables, one for storing string and second for cmaps
@@ -46,21 +53,24 @@ def concat(cnames, ratios=None, discrete=256, name='concat', save=False):
         # Process for the strings
         if isinstance(cname, str):
             colors1 = getattr(cmaps, cname)
-            colors1 = matplotlib.cm.get_cmap(cname)
+            if version.parse(mlp_version) >= version.parse("3.9.0"):
+                colors1 = matplotlib.colormaps[cname]
+            else:
+                colors1 = matplotlib.cm.get_cmap(cname)
             colors1 = colors1(np.linspace(0, 1, int(256 * ratio)))
-            colors1 = Colormap(colors1, name='temp')
-            colors1 = colors1.cut(0.2, 'left')
-            colors1 = colors1.cut(0.2, 'right')
+            colors1 = Colormap(colors1, name="temp")
+            colors1 = colors1.cut(0.2, "left")
+            colors1 = colors1.cut(0.2, "right")
             colors1 = colors1.colors
             colors_str = np.vstack((colors_str, colors1))
         else:
             # Process for the cmaps
-            colors1 = cname(np.linspace(0.2, 0.8,int(256 * ratio)))
+            colors1 = cname(np.linspace(0.2, 0.8, int(256 * ratio)))
             # combine them and build a new colormap
             colors_str = np.vstack((colors_str, colors1))
 
         j = j + 1
-    
+
     mymap = Colormap(colors_str[1:], name=name)
 
     # Discrete logic (Mostly to avoid warning)
@@ -75,48 +85,62 @@ def concat(cnames, ratios=None, discrete=256, name='concat', save=False):
 
     if save is True:
         if isinstance(name, str):
-            np.savetxt(name + '.rgb',
-                       mymap.colors * 255, fmt='%.0f',
-                       header='ncolors=' + str(len(mymap.colors)) + '\n' + 'r g b')
+            np.savetxt(
+                name + ".rgb",
+                mymap.colors * 255,
+                fmt="%.0f",
+                header="ncolors=" + str(len(mymap.colors)) + "\n" + "r g b",
+            )
         else:
             raise Exception("name must be str")
 
     return mymap
 
-def show_cmaps(category, cmap_list):
 
+def show_cmaps(category, cmap_list):
     import matplotlib.pyplot as plt
     import colormaps as cmaps
 
     gradient = np.linspace(0, 1, 256)
     gradient = np.vstack((gradient, gradient))
-    
+
     # Create figure and adjust figure height to number of colormaps
     nrows = len(cmap_list)
     figh = 0.35 + 0.15 + (nrows + (nrows - 1) * 0.1) * 0.22
     fig, axs = plt.subplots(nrows=nrows + 1, figsize=(6.4, figh))
-    fig.subplots_adjust(top=1 - 0.35 / figh, bottom=0.15 / figh,
-                        left=0.2, right=0.99)
-    axs[0].set_title(f'{category} colormaps', fontsize=14)
+    fig.subplots_adjust(top=1 - 0.35 / figh, bottom=0.15 / figh, left=0.2, right=0.99)
+    axs[0].set_title(f"{category} colormaps", fontsize=14)
 
     for ax, name in zip(axs, cmap_list):
-        ax.imshow(gradient, aspect='auto', cmap=eval("cmaps." + name))
-        ax.text(-0.01, 0.5, name, va='center', ha='right', fontsize=10,
-                transform=ax.transAxes)
+        ax.imshow(gradient, aspect="auto", cmap=eval("cmaps." + name))
+        ax.text(
+            -0.01,
+            0.5,
+            name,
+            va="center",
+            ha="right",
+            fontsize=10,
+            transform=ax.transAxes,
+        )
 
     # Turn off *all* ticks & spines, not just the ones with colormaps.
     for ax in axs:
         ax.set_axis_off()
 
+
 def show_cmaps_all():
     print("This may take some time... Please be patient.")
-    CMAPSFILE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'colormaps')
+    CMAPSFILE_DIR = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "colormaps"
+    )
     if CMAPSFILE_DIR is not None:
-        cmapsflist = sorted(glob(os.path.join(CMAPSFILE_DIR, '**/*.rgb'), recursive = True))
+        cmapsflist = sorted(
+            glob(os.path.join(CMAPSFILE_DIR, "**/*.rgb"), recursive=True)
+        )
         FirstTime = True
         for cmap_file in cmapsflist:
-            cname = os.path.basename(cmap_file).split('.rgb')[0]
-            cname_group = cmap_file.split('/')[-2]
+            cname = os.path.basename(cmap_file).split(".rgb")[0]
+            cname_group = cmap_file.split("/")[-2]
             if FirstTime == True:
                 category = cname_group
                 cmap_list = [cname]
@@ -128,16 +152,19 @@ def show_cmaps_all():
                     show_cmaps(category=category, cmap_list=cmap_list)
                     FirstTime = True
 
-def show_cmaps_collection(collection='cartocolors'):
+
+def show_cmaps_collection(collection="cartocolors"):
     print("This may take some time... Please be patient.")
-    CMAPSFILE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'colormaps')
+    CMAPSFILE_DIR = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "colormaps"
+    )
     if CMAPSFILE_DIR is not None:
-        cmapsflist = sorted(glob(os.path.join(CMAPSFILE_DIR, collection, '*.rgb')))
+        cmapsflist = sorted(glob(os.path.join(CMAPSFILE_DIR, collection, "*.rgb")))
         if len(cmapsflist):
             FirstTime = True
             for cmap_file in cmapsflist:
-                cname = os.path.basename(cmap_file).split('.rgb')[0]
-                cname_group = cmap_file.split('/')[-2]
+                cname = os.path.basename(cmap_file).split(".rgb")[0]
+                cname_group = cmap_file.split("/")[-2]
                 if FirstTime == True:
                     category = cname_group
                     cmap_list = [cname]
@@ -145,13 +172,15 @@ def show_cmaps_collection(collection='cartocolors'):
                 else:
                     if category == cname_group:
                         cmap_list.append(cname)
-            
+
             show_cmaps(category=category, cmap_list=cmap_list)
         else:
-            cmapsflist = sorted(glob(os.path.join(CMAPSFILE_DIR, '**/*.rgb'), recursive = True))
+            cmapsflist = sorted(
+                glob(os.path.join(CMAPSFILE_DIR, "**/*.rgb"), recursive=True)
+            )
             FirstTime = True
             for cmap_file in cmapsflist:
-                cname_group = cmap_file.split('/')[-2]
+                cname_group = cmap_file.split("/")[-2]
                 if FirstTime == True:
                     category = cname_group
                     all_collection = [cname_group]
@@ -160,5 +189,7 @@ def show_cmaps_collection(collection='cartocolors'):
                     if category != cname_group:
                         category = cname_group
                         all_collection.append(cname_group)
-            
-            raise ValueError("possible collection names are " + all_collection.__str__())
+
+            raise ValueError(
+                "possible collection names are " + all_collection.__str__()
+            )
--- setup.py
+++ setup.py
@@ -3,16 +3,15 @@ from glob import glob
 from setuptools import setup
 import os
 
-VERSION = '0.4.1'
-CMAPSFILE_DIR = os.path.join('./colormaps/colormaps')
+VERSION = "0.4.2"
+CMAPSFILE_DIR = os.path.join("./colormaps/colormaps")
 
 
-def write_version_py(version=VERSION, filename='colormaps/_version.py'):
-    cnt = '# THIS FILE IS GENERATED FROM SETUP.PY\n' + \
-          '__version__ = "%(version)s"\n'
-    a = open(filename, 'w')
+def write_version_py(version=VERSION, filename="colormaps/_version.py"):
+    cnt = "# THIS FILE IS GENERATED FROM SETUP.PY\n" + '__version__ = "%(version)s"\n'
+    a = open(filename, "w")
     try:
-        a.write(cnt % {'version': version})
+        a.write(cnt % {"version": version})
     finally:
         a.close()
 
@@ -20,94 +19,171 @@ def write_version_py(version=VERSION, filename='colormaps/_version.py'):
 def _listfname():
     l = {}
 
-    l.update({'cartocolors': {
-        'p': 'os.path.join(CMAPSFILE_DIR, "cartocolors", ',
-        'l': sorted(glob(os.path.join(CMAPSFILE_DIR, 'cartocolors/*.rgb')))}})
-    l.update({'cmocean': {
-        'p': 'os.path.join(CMAPSFILE_DIR, "cmocean", ',
-        'l': sorted(glob(os.path.join(CMAPSFILE_DIR, 'cmocean/*.rgb')))}})
-    l.update({'colorbrewer': {
-        'p': 'os.path.join(CMAPSFILE_DIR, "colorbrewer", ',
-        'l': sorted(glob(os.path.join(CMAPSFILE_DIR, 'colorbrewer/*.rgb')))}})
-    l.update({'cubehelix': {
-        'p': 'os.path.join(CMAPSFILE_DIR, "cubehelix", ',
-        'l': sorted(glob(os.path.join(CMAPSFILE_DIR, 'cubehelix/*.rgb')))}})
-    l.update({'ncl': {
-        'p': 'os.path.join(CMAPSFILE_DIR, "ncar_ncl", ',
-        'l': sorted(glob(os.path.join(CMAPSFILE_DIR, 'ncar_ncl/*.rgb')))}})
-    l.update({'scientific': {
-        'p': 'os.path.join(CMAPSFILE_DIR, "scientific", ',
-        'l': sorted(glob(os.path.join(CMAPSFILE_DIR, 'scientific/*.rgb')))}})
-    l.update({'tableau': {
-        'p': 'os.path.join(CMAPSFILE_DIR, "tableau", ',
-        'l': sorted(glob(os.path.join(CMAPSFILE_DIR, 'tableau/*.rgb')))}})
-    l.update({'sciviz': {
-        'p': 'os.path.join(CMAPSFILE_DIR, "sciviz", ',
-        'l': sorted(glob(os.path.join(CMAPSFILE_DIR, 'sciviz/*.rgb')))}})
-    l.update({'colorcet': {
-        'p': 'os.path.join(CMAPSFILE_DIR, "colorcet", ',
-        'l': sorted(glob(os.path.join(CMAPSFILE_DIR, 'colorcet/*.rgb')))}})
-    l.update({'cmasher': {
-        'p': 'os.path.join(CMAPSFILE_DIR, "cmasher", ',
-        'l': sorted(glob(os.path.join(CMAPSFILE_DIR, 'cmasher/*.rgb')))}})
-    l.update({'carbonplan': {
-        'p': 'os.path.join(CMAPSFILE_DIR, "carbonplan", ',
-        'l': sorted(glob(os.path.join(CMAPSFILE_DIR, 'carbonplan/*.rgb')))}})
+    l.update(
+        {
+            "cartocolors": {
+                "p": 'os.path.join(CMAPSFILE_DIR, "cartocolors", ',
+                "l": sorted(glob(os.path.join(CMAPSFILE_DIR, "cartocolors/*.rgb"))),
+            }
+        }
+    )
+    l.update(
+        {
+            "cmocean": {
+                "p": 'os.path.join(CMAPSFILE_DIR, "cmocean", ',
+                "l": sorted(glob(os.path.join(CMAPSFILE_DIR, "cmocean/*.rgb"))),
+            }
+        }
+    )
+    l.update(
+        {
+            "colorbrewer": {
+                "p": 'os.path.join(CMAPSFILE_DIR, "colorbrewer", ',
+                "l": sorted(glob(os.path.join(CMAPSFILE_DIR, "colorbrewer/*.rgb"))),
+            }
+        }
+    )
+    l.update(
+        {
+            "cubehelix": {
+                "p": 'os.path.join(CMAPSFILE_DIR, "cubehelix", ',
+                "l": sorted(glob(os.path.join(CMAPSFILE_DIR, "cubehelix/*.rgb"))),
+            }
+        }
+    )
+    l.update(
+        {
+            "ncl": {
+                "p": 'os.path.join(CMAPSFILE_DIR, "ncar_ncl", ',
+                "l": sorted(glob(os.path.join(CMAPSFILE_DIR, "ncar_ncl/*.rgb"))),
+            }
+        }
+    )
+    l.update(
+        {
+            "scientific": {
+                "p": 'os.path.join(CMAPSFILE_DIR, "scientific", ',
+                "l": sorted(glob(os.path.join(CMAPSFILE_DIR, "scientific/*.rgb"))),
+            }
+        }
+    )
+    l.update(
+        {
+            "tableau": {
+                "p": 'os.path.join(CMAPSFILE_DIR, "tableau", ',
+                "l": sorted(glob(os.path.join(CMAPSFILE_DIR, "tableau/*.rgb"))),
+            }
+        }
+    )
+    l.update(
+        {
+            "sciviz": {
+                "p": 'os.path.join(CMAPSFILE_DIR, "sciviz", ',
+                "l": sorted(glob(os.path.join(CMAPSFILE_DIR, "sciviz/*.rgb"))),
+            }
+        }
+    )
+    l.update(
+        {
+            "colorcet": {
+                "p": 'os.path.join(CMAPSFILE_DIR, "colorcet", ',
+                "l": sorted(glob(os.path.join(CMAPSFILE_DIR, "colorcet/*.rgb"))),
+            }
+        }
+    )
+    l.update(
+        {
+            "cmasher": {
+                "p": 'os.path.join(CMAPSFILE_DIR, "cmasher", ',
+                "l": sorted(glob(os.path.join(CMAPSFILE_DIR, "cmasher/*.rgb"))),
+            }
+        }
+    )
+    l.update(
+        {
+            "carbonplan": {
+                "p": 'os.path.join(CMAPSFILE_DIR, "carbonplan", ',
+                "l": sorted(glob(os.path.join(CMAPSFILE_DIR, "carbonplan/*.rgb"))),
+            }
+        }
+    )
     return l
 
 
-def write_cmaps(template_file='./cmaps.template'):
-    with open(template_file, 'rt') as f:
+def write_cmaps(template_file="./cmaps.template"):
+    with open(template_file, "rt") as f:
         c = f.read()
     l = _listfname()
     for t in l.keys():
-        for cmap_file in l[t]['l']:
-            cname = os.path.basename(cmap_file).split('.rgb')[0]
+        for cmap_file in l[t]["l"]:
+            cname = os.path.basename(cmap_file).split(".rgb")[0]
             # start with the number will result illegal attribute
-            if cname[0].isdigit() or cname.startswith('_'):
-                cname = 'N' + cname
-            if '-' in cname:
-                cname = cname.replace('-', '_')
-            if '+' in cname:
-                cname = cname.replace('+', '_')
-            c += '    @property\n'
-            c += '    def {}(self):\n'.format(cname)
+            if cname[0].isdigit() or cname.startswith("_"):
+                cname = "N" + cname
+            if "-" in cname:
+                cname = cname.replace("-", "_")
+            if "+" in cname:
+                cname = cname.replace("+", "_")
+            c += "    @property\n"
+            c += "    def {}(self):\n".format(cname)
             c += '        cname = "{}"\n'.format(cname)
             c += '        cmap_file = {} "{}")\n'.format(
-                l[t]['p'], os.path.basename(cmap_file))
-            c += '        cmap = Colormap(self._coltbl(cmap_file), name=cname)\n'
+                l[t]["p"], os.path.basename(cmap_file)
+            )
+            c += "        cmap = Colormap(self._coltbl(cmap_file), name=cname)\n"
             c += '        if version.parse(mlp_version) >= version.parse("3.6.0"):\n'
-            c += '            if cname in sorted(_colormaps):\n'
-            c += '                return matplotlib.cm.get_cmap(cname)\n'
-            c += '            else:\n'
-            c += '                matplotlib.colormaps.register(name=cname, cmap=cmap)\n'
-            c += '        else:\n'
-            c += '            if cname in matplotlib.cm._cmap_registry:\n'
-            c += '                return matplotlib.cm.get_cmap(cname)\n'
-            c += '            else:\n'
-            c += '                matplotlib.cm.register_cmap(name=cname, cmap=cmap)\n\n'
-            c += '        return cmap\n\n'
+            c += "            if cname in sorted(_colormaps):\n"
+            c += '                if version.parse(mlp_version) >= version.parse("3.9.0"):\n'
+            c += "                    return  matplotlib.colormaps[cname]\n"
+            c += "                else: \n"
+            c += "                    return matplotlib.cm.get_cmap(cname)\n"
+            c += "            else:\n"
+            c += (
+                "                matplotlib.colormaps.register(name=cname, cmap=cmap)\n"
+            )
+            c += "        else:\n"
+            c += "            if cname in matplotlib.cm._cmap_registry:\n"
+            c += '                if version.parse(mlp_version) >= version.parse("3.9.0"):\n'
+            c += "                    return  matplotlib.colormaps[cname]\n"
+            c += "                else: \n"
+            c += "                    return matplotlib.cm.get_cmap(cname)\n"
+            c += "            else:\n"
+            c += (
+                "                matplotlib.cm.register_cmap(name=cname, cmap=cmap)\n\n"
+            )
+            c += "        return cmap\n\n"
 
-            c += '    @property\n'
-            c += '    def {}(self):\n'.format(cname + '_r')
-            c += '        cname = "{}"\n'.format(cname + '_r')
+            c += "    @property\n"
+            c += "    def {}(self):\n".format(cname + "_r")
+            c += '        cname = "{}"\n'.format(cname + "_r")
             c += '        cmap_file = {} "{}")\n'.format(
-                l[t]['p'], os.path.basename(cmap_file))
-            c += '        cmap = Colormap(self._coltbl(cmap_file)[::-1], name=cname)\n'
+                l[t]["p"], os.path.basename(cmap_file)
+            )
+            c += "        cmap = Colormap(self._coltbl(cmap_file)[::-1], name=cname)\n"
             c += '        if version.parse(mlp_version) >= version.parse("3.6.0"):\n'
-            c += '            if cname in sorted(_colormaps):\n'
-            c += '                return matplotlib.cm.get_cmap(cname)\n'
-            c += '            else:\n'
-            c += '                matplotlib.colormaps.register(name=cname, cmap=cmap)\n'
-            c += '        else:\n'
-            c += '            if cname in matplotlib.cm._cmap_registry:\n'
-            c += '                return matplotlib.cm.get_cmap(cname)\n'
-            c += '            else:\n'
-            c += '                matplotlib.cm.register_cmap(name=cname, cmap=cmap)\n\n'
-            c += '        return cmap\n\n'
+            c += "            if cname in sorted(_colormaps):\n"
+            c += '                if version.parse(mlp_version) >= version.parse("3.9.0"):\n'
+            c += "                    return  matplotlib.colormaps[cname]\n"
+            c += "                else: \n"
+            c += "                    return matplotlib.cm.get_cmap(cname)\n"
+            c += "            else:\n"
+            c += (
+                "                matplotlib.colormaps.register(name=cname, cmap=cmap)\n"
+            )
+            c += "        else:\n"
+            c += "            if cname in matplotlib.cm._cmap_registry:\n"
+            c += '                if version.parse(mlp_version) >= version.parse("3.9.0"):\n'
+            c += "                    return  matplotlib.colormaps[cname]\n"
+            c += "                else: \n"
+            c += "                    return matplotlib.cm.get_cmap(cname)\n"
+            c += "            else:\n"
+            c += (
+                "                matplotlib.cm.register_cmap(name=cname, cmap=cmap)\n\n"
+            )
+            c += "        return cmap\n\n"
 
-    cmapspy = './colormaps/cmaps.py'
-    with open(cmapspy, 'wt') as fw:
+    cmapspy = "./colormaps/cmaps.py"
+    with open(cmapspy, "wt") as fw:
         fw.write(c)
 
 
@@ -115,25 +191,32 @@ write_version_py()
 write_cmaps()
 
 setup(
-    name='colormaps',
-    author='Pratiman Patel',
+    name="colormaps",
+    author="Pratiman Patel",
     version=VERSION,
-    author_email='pratiman_patel@hotmail.com',
-    packages=['colormaps', ],
-    package_data={'colormaps': ['colormaps/ncar_ncl/*',
-                            'colormaps/cartocolors/*',
-                            'colormaps/cmocean/*',
-                            'colormaps/colorbrewer/*',
-                            'colormaps/cubehelix/*',
-                            'colormaps/scientific/*',
-                            'colormaps/tableau/*',
-                            'colormaps/sciviz/*',
-                            'colormaps/colorcet/*',
-                            'colormaps/cmasher/*',
-                            'colormaps/carbonplan/*'], },
-    data_files=[('', ['cmaps.template', 'LICENSE']),],
-    url='https://pratiman-91.github.io/colormaps/',
-    license='LICENSE',
-    description='A collection of colormaps or color palettes for Python',
-    install_requires=['matplotlib', 'numpy'],
+    author_email="pratiman_patel@hotmail.com",
+    packages=[
+        "colormaps",
+    ],
+    package_data={
+        "colormaps": [
+            "colormaps/ncar_ncl/*",
+            "colormaps/cartocolors/*",
+            "colormaps/cmocean/*",
+            "colormaps/colorbrewer/*",
+            "colormaps/cubehelix/*",
+            "colormaps/scientific/*",
+            "colormaps/tableau/*",
+            "colormaps/sciviz/*",
+            "colormaps/colorcet/*",
+            "colormaps/cmasher/*",
+            "colormaps/carbonplan/*",
+        ],
+    },
+    data_files=[
+        ("", ["cmaps.template", "LICENSE"]),
+    ],
+    url="https://pratiman-91.github.io/colormaps/",
+    description="A collection of colormaps or color palettes for Python",
+    install_requires=["matplotlib", "numpy"],
 )
